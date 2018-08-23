ef82cfcfb90a0e41bb88f94db49f67afeb1073cb
Zheng Cao <zheng.cao@milliman.com>
2018-08-23 09:21:56 -0700

    Convert dataset from wide to long

--- dataWrangling.R
+++ dataWrangling.R
@@ -6,3 +6,6 @@ library(stringr)
 ssa <- read_csv("http://594442.youcanlearnit.net/ssadisability.csv")
 glimpse(ssa)
 
+# Convert dataset from wide to long
+ssa_long <- gather(ssa, month, applications, -Fiscal_Year)
+print(ssa_long, n = 20)
